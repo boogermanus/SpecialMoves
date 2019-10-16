bf687dc5c55e2ca58bac35915b81e3861da9dfb9
Jordan Woodruff <jordan@neko-chan.local>
2019-10-15 21:39:02 -0500

    new elements and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,12 @@ import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: any;
+  let compiled: any;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -14,11 +16,14 @@ describe('AppComponent', () => {
       imports: [
         MatFormFieldModule,
         MatSelectModule,
+        BrowserAnimationsModule,
        ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.debugElement.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
@@ -31,11 +36,20 @@ describe('AppComponent', () => {
 
   it('should render title in a h1 tag', () => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(`Welcome to ${app.title}!`);
   });
 
   it('should have property firstLetter', () => {
     expect(app.firstLetter).toBeTruthy();
   });
+
+  it(`should have mat-label with id 'lblFirstLetter'`, () => {
+    expect(compiled.querySelector('#lblFirstLetter').id).toEqual('lblFirstLetter');
+  });
+
+  it(`should have mat-label with id 'lblFirstLetter' and InnerText = 'First Letter of First Name'`, () => {
+    expect(compiled.querySelector('#lblFirstLetter').innerText).toEqual('First Letter of First Name');
+  });
+
+
 });
