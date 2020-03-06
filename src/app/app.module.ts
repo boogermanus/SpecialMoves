85fbbb3b0ca1300c78e37d74641c6081b328ae52
Jordan Woodruff <boogermanus@gmail.com>
2020-03-05 21:50:44 -0600

    Revert "material module"
    This reverts commit 5cdafbac4df7ff8f4e9c79ca54c55300a69baeae.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
     FormsModule
   ],
   providers: [],
