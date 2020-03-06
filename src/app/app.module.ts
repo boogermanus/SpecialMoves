13f1ca2edaab0ea424d99a24ee5cf14c5877fb80
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-03-06 08:43:12 -0600

    Revert "Angular 9...gulp"
    This reverts commit 5e221366341f650eb92aa34a494d7fbb04474059.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +14,9 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
     FormsModule
   ],
   providers: [],
