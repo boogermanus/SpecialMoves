9e1be72a32fb04420eec6a97716b6a147ad37967
Jordan Woodruff <boogermanus@gmail.com>
2025-06-16 21:14:34 -0500

    start store

---
+++ src/app/move-store.ts
@@ -0,0 +1,12 @@
+﻿type MoveState = {
+  firstLetter: string;
+  dayOfBirth: number | null;
+  surnameLetter: string;
+}
+
+const initialMoveState: MoveState = {
+  firstLetter: '',
+  dayOfBirth: null,
+  surnameLetter: ''
+}
+
