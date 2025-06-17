5a43a3c1fbd6a45d781479b7856a350a7a6e3312
Jordan Woodruff <boogermanus@gmail.com>
2025-06-16 20:29:15 -0500

    angular19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,10 @@ import { LetterMove } from './interfaces/LetterMove';
 import { NumberMove } from './interfaces/NumberMove';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent {
   public title = 'Special Move';
